17e0716e9bc7eeba9c89571848f7be60cab9b938
Ned Ruggeri <ruggeri@self-loop.com>
2013-02-19 13:25:33 -0800

    expand greatly

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var Asteroids = (function () {
-  function Asteroid (x, y) {
+  function Asteroid (startX, startY, vel) {
     var that = this;
 
-    that.x = x;
-    that.y = y;
+    that.x = startX;
+    that.y = startY;
 
     that.draw = function (ctx) {
       ctx.fillStyle = "#000";
@@ -12,34 +12,116 @@ var Asteroids = (function () {
       ctx.arc(that.x, that.y, Asteroid.RADIUS, 0, 2 * Math.PI, true);
       ctx.fill();
     }
+
+    that.update = function () {
+      that.x += vel.x;
+      that.y += vel.y;
+    }
+
+    that.outOfBounds = function (dimX, dimY) {
+      return (that.x < 0)
+        || (that.y < 0)
+        || (that.x > dimX)
+        || (that.y > dimY);
+    }
   }
 
   Asteroid.RADIUS = 25;
-  Asteroid.randomAsteroid = function (dimX, dimY) {
-    return new Asteroid(dimX * Math.random(), dimY * Math.random());
+  Asteroid.randomAsteroid = function (dimX, dimY, speed) {
+    return new Asteroid(
+      dimX * Math.random(),
+      dimY * Math.random(),
+      { x: speed * Math.random(), y: speed * Math.random() }
+    );
   };
+
+  function Ship (startX, startY) {
+    var that = this;
+
+    that.x = startX;
+    that.y = startY;
+
+    that.vel = { x: 0, y: 0 };
+
+    that.draw = function (ctx) {
+      ctx.fillStyle = "#F00"
+
+      ctx.beginPath();
+      ctx.arc(that.x, that.y, Ship.RADIUS, 0, 2 * Math.PI, true);
+      ctx.fill();
+    };
+
+    that.update = function () {
+      that.x += that.vel.x;
+      that.y += that.vel.y;
+    };
+
+    that.isCollided = function (asteroid) {
+      var dist = Math.sqrt(
+        Math.pow((that.x - asteroid.x), 2) +
+          Math.pow((that.y - asteroid.y), 2));
+
+      return (dist - (Ship.RADIUS + Asteroid.RADIUS)) < 0;
+    };
+  }
+
+  Ship.RADIUS = 15;
 
   function Game (ctx) {
     var that = this;
 
+    that.timerId = null;
+
+    that.ship = new Ship(Game.DIM_X / 2, Game.DIM_Y / 2);
     that.asteroids = _.times(10, function () {
-      return Asteroid.randomAsteroid(Game.DIM_X, Game.DIM_Y);
+      return Asteroid.randomAsteroid(Game.DIM_X, Game.DIM_Y, 10);
     });
+
+    that.checkCollisions = function () {
+      _.each(that.asteroids, function (asteroid) {
+        if (that.ship.isCollided(asteroid)) {
+          alert("You died!");
+          that.stop();
+        }
+      });
+    }
+
+    that.update = function () {
+      that.ship.update();
+
+      _.each(that.asteroids, function (asteroid) {
+        asteroid.update();
+      });
+
+      // remove off-screen asteroids
+      that.asteroids = _.filter(that.asteroids, function (asteroid) {
+        return !asteroid.outOfBounds(Game.DIM_X, Game.DIM_Y);
+      });
+    }
 
     that.draw = function () {
       ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+
+      that.ship.draw(ctx);
 
       _.each(that.asteroids, function (asteroid) {
         asteroid.draw(ctx);
       });
     };
 
-    FPS = 2;
+    FPS = 32;
     that.start = function () {
-      setInterval(function () {
+      that.timerId = setInterval(function () {
+        that.update();
         that.draw();
+
+        that.checkCollisions();
       }, 1000 / FPS);
     };
+
+    that.stop = function () {
+      clearInterval(that.timerId);
+    }
   }
 
   Game.DIM_X = 1000;
