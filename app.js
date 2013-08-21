9971bf9852048b3779226bcf4022e1711eea4d8a
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-21 10:19:17 -0700

    fixes fixes fixes

--- app.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  var canvas = $("<canvas width='" + Asteroids.Game.DIM_X + 
-                 "' height='" + Asteroids.Game.DIM_Y + "'></canvas>");
-  $('body').append(canvas);
-
-  var ctx = canvas.get(0).getContext("2d");
-  new Asteroids.Game(ctx).start();
-});
